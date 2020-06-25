4a06207eac8a136bdbf1ced85931272ffe9cebc6
GnanaKushwanthDoredla <doredlagnanakushwanth@gmail.com>
2020-06-25 21:10:49 +0530

    added

--- display.php
+++ display.php
@@ -1,7 +1,6 @@
 <html>
 <body>
-hello
-<?php echo $_POST["Name"]; ?><br>
+hello <?php echo $_POST["Name"]; ?><br>
 contactnumbe:<?php echo $_POST["contact number"]; ?><br>
 course details:<?php echo $_POST["course"]; ?><br>
 subjects:<?php echo $_POST["subjects"]; ?><br>
